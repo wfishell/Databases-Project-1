d563fae789134b4218905e6c0d9618cec093b806
wfishell <wf2322@columbia.edu>
2024-11-01 15:52:00 -0400

    Create UserProfile.py

---
+++ UserProfile.py
@@ -0,0 +1,8 @@
+'''
+This page should contain on information regarding the user entity
+list of projects user is managing
+list of projects user is a member of
+Create Project Button
+Search bar for searching for projects
+Homepage link( Note this is the homepage)
+'''
